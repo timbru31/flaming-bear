94ab49b6125c76752dace4e104b44f3c37121ed1
Tim Brust <tim.brust@sinnerschrader.com>
2014-06-04 12:47:37 +0200

    WIP need to fix "Hockenheimring"

--- winnerBot.rb
+++ winnerBot.rb
@@ -1,20 +1,18 @@
 require File.join(File.dirname(__FILE__), 'botlib')
-require 'pry'
 
 class WinnerBot
 
   attr_accessor :botname, :icon
 
   def initialize
-    @botname = "Winner"
-    @icon = "X"
+    @botname = "Tim"
   end
 
   def onTurn(lib)
     # Stuff
     vehicle = lib.getVehicle
     track = lib.getTrack
-    vehicle["thrust"] = 10
+    vehicle["thrust"] = 5
     x = vehicle["x"]
     y = vehicle["y"]
     wall = false
@@ -35,32 +33,29 @@ class WinnerBot
       nextRacePointDistance = lib.calcDistance(vehicle, nextRacePoint)
       nextSpeedUpPointDistance = lib.calcDistance(vehicle, nextSpeedUpPoint)
       if nextSpeedUpPointDistance < nextRacePointDistance
-        if lib.facing?(direction, speedUpDirection)
-          puts "speed"
+        if facing?(direction, speedUpDirection) && (nextRacePointDistance - nextSpeedUpPointDistance > 30)
+          puts "speedpoint"
           nextPoint = nextSpeedUpPoint
         end
-        # if nextSpeedUpPointDistance < 45
-        #   puts "reduce"
-        #   vehicle["thrust"] = 5
-        # end
+        puts nextRacePointDistance
       end
     end
 
 
-    # Maybe we need a wall first?
-    unless nextWall.nil?
-      wallDirection = lib.calcDirection(vehicle, nextWall)
-      nextPointDistance = lib.calcDistance(vehicle, nextRacePoint)
-      nextWallDistance = lib.calcDistance(vehicle, nextWall)
-      if nextWallDistance < nextPointDistance
-        # Before//Behind check
-        if lib.facing?(direction, wallDirection)
-          puts "wall"
-          nextPoint = nextWall
-          wall = true
-        end
-      end
-    end
+    # # Maybe we need a wall first?
+    # unless nextWall.nil?
+    #   wallDirection = lib.calcDirection(vehicle, nextWall)
+    #   nextPointDistance = lib.calcDistance(vehicle, nextRacePoint)
+    #   nextWallDistance = lib.calcDistance(vehicle, nextWall)
+    #   if nextWallDistance < nextPointDistance
+    #     # Before//Behind check
+    #     if facing?(direction, wallDirection)
+    #       puts "wall"
+    #       nextPoint = nextWall
+    #       wall = true
+    #     end
+    #   end
+    # end
 
     # Find fastest way with corners
     nextPointCorners = lib.getPointWithCorners nextPoint
@@ -70,16 +65,25 @@ class WinnerBot
       end
     end
 
-    # walls = lib.getWalls
-    # walls.each do |wall|
-    #   if ( ((wall["x"].abs - x).abs <= 25) && ((wall["y"].abs - y).abs <= 25) )
-    #     puts "hang"
-    #     direction = lib.calcDirection vehicle, nextPoint
-    #     direction < 270 ? (direction = direction + 35) : (direction = direction - 35)
-    #     vehicle["direction"] = direction
-    #     return vehicle
-    #   end
-    # end
+    walls = lib.getWalls
+    walls.each do |wall|
+      if ( ((wall["x"].abs - x).abs <= 50) && ((wall["y"].abs - y).abs <= 50) )
+        puts "hang"
+        nextPointDistance = lib.calcDistance(vehicle, nextPoint)
+        direction = lib.calcDirection vehicle, nextPoint
+        puts lib.calcDistance(vehicle, nextPoint)
+        if nextPointDistance > 90 && (direction.ceil <= 95 && direction.ceil >= 85)
+          bump = 45
+        else
+          nextPointDistance < 50 ? bump = 30 : bump = 35
+          nextPointDistance < 40 ? bump = 25 : bump
+        end
+        puts bump
+        direction > 270 ?  direction = direction - bump : direction = direction + bump
+        vehicle["direction"] = direction.ceil
+        return vehicle
+      end
+    end
 
     direction = lib.calcDirection vehicle, nextPoint
     vehicle["direction"] = direction.ceil
@@ -100,8 +104,24 @@ class WinnerBot
     return vehicle
   end
 
+
+  def facing?(vehicleDirection, pointDirection)
+    (vehicleDirection.abs - pointDirection).abs <= 25
+  end
+
+end
+
+address = "127.0.0.1"
+port = 8124
+
+if ARGV.length == 1
+  address = ARGV[0]
+end
+
+if ARGV.length == 2
+  address = ARGV[0]
+  port = ARGV[1]
 end
 
 bot = Bot.new
-bot.start(WinnerBot.new)
-
+bot.start(WinnerBot.new, address, port)
