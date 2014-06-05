27c2a132a6033f5fa04825ce21a5ff5efb80930c
Tim Brust <tim.brust@sinnerschrader.com>
2014-06-05 18:44:42 +0200

    removed debug

--- winnerBot.rb
+++ winnerBot.rb
@@ -34,28 +34,10 @@ class WinnerBot
       nextSpeedUpPointDistance = lib.calcDistance(vehicle, nextSpeedUpPoint)
       if nextSpeedUpPointDistance < nextRacePointDistance
         if facing?(direction, speedUpDirection) && (nextRacePointDistance - nextSpeedUpPointDistance > 30)
-          puts "speedpoint"
           nextPoint = nextSpeedUpPoint
         end
-        puts nextRacePointDistance
       end
     end
-
-
-    # # Maybe we need a wall first?
-    # unless nextWall.nil?
-    #   wallDirection = lib.calcDirection(vehicle, nextWall)
-    #   nextPointDistance = lib.calcDistance(vehicle, nextRacePoint)
-    #   nextWallDistance = lib.calcDistance(vehicle, nextWall)
-    #   if nextWallDistance < nextPointDistance
-    #     # Before//Behind check
-    #     if facing?(direction, wallDirection)
-    #       puts "wall"
-    #       nextPoint = nextWall
-    #       wall = true
-    #     end
-    #   end
-    # end
 
     # Find fastest way with corners
     nextPointCorners = lib.getPointWithCorners nextPoint
@@ -68,17 +50,14 @@ class WinnerBot
     walls = lib.getWalls
     walls.each do |wall|
       if ( ((wall["x"].abs - x).abs <= 50) && ((wall["y"].abs - y).abs <= 50) )
-        puts "hang"
         nextPointDistance = lib.calcDistance(vehicle, nextPoint)
         direction = lib.calcDirection vehicle, nextPoint
-        puts lib.calcDistance(vehicle, nextPoint)
         if nextPointDistance > 90 && (direction.ceil <= 95 && direction.ceil >= 85)
           bump = 45
         else
-          nextPointDistance < 50 ? bump = 30 : bump = 35
+          nextPointDistance < 50 ? bump = 30 : bump = 40
           nextPointDistance < 40 ? bump = 25 : bump
         end
-        puts bump
         direction > 270 ?  direction = direction - bump : direction = direction + bump
         vehicle["direction"] = direction.ceil
         return vehicle
@@ -87,19 +66,6 @@ class WinnerBot
 
     direction = lib.calcDirection vehicle, nextPoint
     vehicle["direction"] = direction.ceil
-
-    # if track["racepoints"].length > 0
-    #   targetPoint = lib.getCoordsNextRacepoint
-    # end
-
-    # if !targetPoint.nil?
-    #   direction = lib.calcDirection vehicle, targetPoint
-    # end
-
-    # if !vehicle.nil?
-    #   vehicle["direction"] = direction.ceil
-    #   vehicle["thrust"] = 100
-    # end
 
     return vehicle
   end
